459ea115e8333dff66c0d5671b964aab99aeca8d
DennisMaes5 <56113502+DennisMaes5@users.noreply.github.com>
2019-10-03 11:47:06 +0200

    Jipla

---
+++ app.js
@@ -0,0 +1,28 @@
+var constraints = { video: { facingMode: "user"}, audio: false};
+const cameraView = document.querySelector("#cameraView"),
+    cameraOutput = document.querySelector("#cameraOutput"),
+    cameraSensor = document.querySelector("#cameraSensor"),
+    cameraTrigger = document.querySelector("#cameraTrigger");
+
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function (stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error){
+            console.error("Oopsie, iets kapoet", error);
+            }
+        );
+}
+
+cameraTrigger.onclick = function () {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+window.addEventListener("load", cameraStart, false);
